ded5cbe5150d403b98a1955c67615c9bb1dfd507
Mai Cong Ngoan <ngoanmc@hybrid-technologies.vn>
2022-06-27 15:35:12 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -8,26 +8,7 @@
           <router-view></router-view>
         <!-- </keep-alive> -->
 <!--        </keep-alive>-->
-        <div class="test-api-wp">
-          <table class="table is-bordered">
-            <thead>
-            <tr>
-              <th>Title</th>
-              <th>Posted at</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="post in posts" :key="post.id">
-              <td>{{post.title.rendered}}</td>
-              <td>{{post.date_gmt}}</td>
-            </tr>
-            </tbody>
-          </table>
-          <pagination :pagination="pagination"
-                      @prev="--postsData.page; getPosts();"
-                      @next="postsData.page++; getPosts();">
-          </pagination>
-        </div>
+
 
       </div>
     </div>
@@ -36,58 +17,11 @@
 
 <script>
 import Header from './components/particle/Header'
-import Pagination from './components/pagination/Pagination'
-import axios from "axios";
 export default {
   name: 'App',
-  data() {
-    return {
-      postsUrl: 'https://dhweb.vn/wp-json/wp/v2/posts',
-      posts: [],
-      postsData: {
-        per_page: 5,
-        page: 1
-      },
-      pagination: {
-        prevPage: '',
-        nextPage: '',
-        totalPages: '',
-        from: '',
-        to: '',
-        total: '',
-      },
-    }
-  },
   components: {
     Header,
-    Pagination
   },
-  mounted() {
-    this.getPosts();
-  },
-  methods: {
-    getPosts() {
-      axios.get(this.postsUrl, {params: this.postsData})
-          .then((response) => {
-            this.posts = response.data;
-            this.configPagination(response.headers);
-            console.log(this.posts)
-          })
-          .catch( (error) => {
-            console.log(error);
-          });
-    },
-    configPagination(headers) {
-      this.pagination.total = headers['x-wp-total'];
-      this.pagination.totalPages = headers['x-wp-totalpages'];
-      this.pagination.from = this.pagination.total ? ((this.postsData.page - 1) * this.postsData.per_page) + 1 : ' ';
-      this.pagination.to = (this.postsData.page * this.postsData.per_page) > this.pagination.total ? this.pagination.total : this.postsData.page * this.postsData.per_page;
-      this.pagination.prevPage = this.postsData.page > 1 ? this.postsData.page : '';
-      this.pagination.nextPage = this.postsData.page < this.pagination.totalPages ? this.postsData.page + 1 : '';
-
-      console.log('Total: ' + this.pagination.total + 'Total Pages' + this.pagination.totalPages)
-    }
-  }
 
 }
 </script>
--- src/components/product_manager/ProductManagerList.vue
+++ src/components/product_manager/ProductManagerList.vue
@@ -2,6 +2,7 @@
   <div class="row" v-if="isAuthencation">
     <div class="row title-page">
       <h3>List Product</h3>
+      {{'Total Item: ' + pagination.totalItem + ' Current Page: ' + pagination.curPage}}
     </div>
     <div class="row">
       <router-link :to="{ name: 'product.create' }"><a class="waves-effect waves-light btn-small">Add new</a></router-link>
@@ -15,6 +16,19 @@
           <th>Action</th>
         </tr>
       </thead>
+      <div class="preloader-wrapper small active" v-if="!isLoading">
+        <div class="spinner-layer spinner-green-only">
+          <div class="circle-clipper left">
+            <div class="circle"></div>
+          </div>
+          <div class="gap-patch">
+            <div class="circle"></div>
+          </div>
+          <div class="circle-clipper right">
+            <div class="circle"></div>
+          </div>
+        </div>
+      </div>
       <ProductManagerItem
         v-for="item in products"
         :key="item.invId"
@@ -23,43 +37,90 @@
       >
       </ProductManagerItem>
     </table>
+<!--    USE @input="onchange"-->
+<!--    <v-pagination v-model="pagination.curPage" :page-count="pagination.totalPage" @input="onPageChange"></v-pagination>-->
+<!--    USE WATCH curPage-->
+    <v-pagination v-model="pagination.curPage" :page-count="pagination.totalPage"></v-pagination>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
-
+import axios from "axios";
+import vPagination from 'vue-plain-pagination'
 import ProductManagerItem from "./ProductManagerItem.vue";
 export default {
   name: "ProductManagerList",
 
   components: {
     ProductManagerItem,
+    vPagination
   },
 
   data() {
-    return {};
+    return {
+      isLoading: false,
+      products: [],
+      pagination: {
+        totalItem: 0,
+        curPage: 1,
+        perPage: 5,
+        totalPage: 1
+      }
+    };
   },
 
   computed: {
-    ...mapGetters(["products","isAuthencation"]),
+    ...mapGetters(["isAuthencation"]),
   },
 
   created() {
-    this.getAllProduct();
+    this.fetchAll();
+    this.fetch();
   },
 
   mounted() {
+
   },
 
   methods: {
-    ...mapActions(["getAllProduct", "getAuthen"]),
+    ...mapActions(["getAuthen"]),
+    async fetchAll(){
+      try {
+        const resAll = await axios.get(`https://6177a06f9c328300175f5a35.mockapi.io/products`)
+        this.pagination.totalItem = resAll.data.length
+        this.pagination.totalPage = Math.ceil(this.pagination.totalItem / this.pagination.perPage)
+      } catch (e) {
+        console.log(e)
+      } finally {
+        this.isLoading = true
+      }
+
+    },
+    async fetch(){
+      this.isLoading = false
+      try {
+        this.products = []
+        const resPage = await axios.get(`https://6177a06f9c328300175f5a35.mockapi.io/products?page=${this.pagination.curPage}&limit=${this.pagination.perPage}`)
+        this.products = resPage.data
+      } catch (e) {
+        console.log(e)
+      } finally {
+        this.isLoading = true
+      }
+    },
+    // onPageChange(){
+    //   this.fetch();
+    // },
   },
   watch: {
     isAuthencation(){
       if(!this.isAuthencation) {
         this.$router.push({name: 'product.list'})
       }
+    },
+    'pagination.curPage'(){
+      this.fetch();
     }
   }
 };
